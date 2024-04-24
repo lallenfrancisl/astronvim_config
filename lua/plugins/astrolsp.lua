df753555b5d3837cdf92bcc70c36c5b2398826b4
Allen Francis <lallenfrancisl@gmail.com>
2024-04-24 13:22:55 +0530

    Update confg

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -44,6 +44,22 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      -- tsserver = {
+      --   init_options = {
+      --     plugins = {
+      --       {
+      --         name = "@vue/typescript-plugin",
+      --         location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+      --         languages = { "javascript", "typescript", "vue" },
+      --       },
+      --     },
+      --   },
+      --   filetypes = {
+      --     "javascript",
+      --     "typescript",
+      --     "vue",
+      --   },
+      -- },
     },
     -- customize how language servers are attached
     handlers = {
